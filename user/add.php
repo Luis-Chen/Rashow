b6e2da749a724807303c606473e2c6fd88121eca
leo5916267 <leo5916267@gmail.com>
2017-05-12 21:28:26 +0800

    asd

--- user/add.php
+++ user/add.php
@@ -92,8 +92,10 @@
 
         }else{
           $insert = $connect -> prepare("INSERT INTO message (mbid,title,text,date)
-                                                                          VALUES(?,?,?,?)");
-          $insert -> execute(array($userInfo['mbid'],$userInfo['title'],$userInfo['text'],$userInfo['toDay']));
+                                                                          VALUES(:id,:title,:txt,:da)");
+          $insert -> bindValue(':title',$userInfo['title'], PDO::PARAM_STR);
+          $insert -> bindValue(':txt',$userInfo['text'], PDO::PARAM_STR);
+          $insert -> execute(array(':id' =>$userInfo['mbid'] ,':da' =>$userInfo['toDay'] ));
           echo "<b>發信成功!!</b>";
 
         }
