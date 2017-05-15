1aad5f266d66f5d84d7084bd454987446ca284f0
leo5916267 <leo5916267@gmail.com>
2017-05-15 19:26:39 +0800

    asd

--- appear/index.php
+++ appear/index.php
@@ -46,11 +46,23 @@
           <?php endforeach; ?>
         </div>
         <!-- Controls -->
-        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+        <script type="text/javascript">
+        document.onkeydown = function(){
+          if(event.keyCode== 37){
+          //key code 37為→
+             $("a#right").load(); 
+          }
+          else if(event.keyCode== 39){
+            //key code 39為←
+              $("a#left").load();
+          }
+        }
+        </script>
+        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev" id="left">
           <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
           <span class="sr-only">Previous</span>
         </a>
-        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next" id="right">
           <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
           <span class="sr-only">Next</span>
         </a>
